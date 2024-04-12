33807e954eb61654f1cefcb891643133ca1c6b12
DreaMSJR <leomeng27@gmail.com>
2024-04-12 21:16:56 +0800

    update

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -10,9 +10,10 @@
 // I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    fill_vec(&mut vec0);
+    let mut vec1 = vec0.clone();
 
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
@@ -21,12 +22,12 @@ fn main() {
     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &mut Vec<i32>) {
+    // let mut vec = vec;
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    // vec
 }
